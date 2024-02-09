1f4aca6e8b32806f1c123488128d81814d9d93c3
anibalfn <anibalneto29@gmail.com>
2024-02-09 02:42:47 -0300

    Add X button

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
-import { ptBR } from "date-fns/locale";
+import { X } from 'lucide-react';
 
 interface NoteCardProps {
   note: {
@@ -14,7 +14,7 @@ export function NoteCard({ note }: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md text-left bg-slate-800 flex flex-col p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
         <span className="text-sm font-medium text-slate-400">
-          {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+          {formatDistanceToNow(note.date, { addSuffix: true })}
         </span>
         <p className="text-sm leading-6 text-slate-300">{note.content}</p>
 
@@ -22,12 +22,15 @@ export function NoteCard({ note }: NoteCardProps) {
       </Dialog.Trigger>
 
       <Dialog.Portal>
+
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
         <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400">
+            <X className="size-5" />
+          </Dialog.Close>
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-200">
               {formatDistanceToNow(note.date, {
-                locale: ptBR,
                 addSuffix: true,
               })}
             </span>
@@ -41,6 +44,7 @@ export function NoteCard({ note }: NoteCardProps) {
             Do you want to <span className="text-red-400 group-hover:underline">delete this note</span>?
           </button>
         </Dialog.Content>
+
       </Dialog.Portal>
     </Dialog.Root>
   );
