00e04c8dfcc4c53486859e68b186a2e5285c2792
anibalfn <anibalneto29@gmail.com>
2024-02-09 02:34:11 -0300

    Code modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -12,9 +12,9 @@ interface NoteCardProps {
 export function NoteCard({ note }: NoteCardProps) {
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="rounded-md bg-slate-800 p-5 gap-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 text-left flex flex-col focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
-        <span className="text-sm font-medium text-slate-200">
-          {formatDistanceToNow(note.date)}
+      <Dialog.Trigger className="rounded-md text-left bg-slate-800 flex flex-col p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+        <span className="text-sm font-medium text-slate-400">
+          {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
         </span>
         <p className="text-sm leading-6 text-slate-300">{note.content}</p>
 
@@ -22,15 +22,25 @@ export function NoteCard({ note }: NoteCardProps) {
       </Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-m flex flex-col outline-none">
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-200">
-              {formatDistanceToNow(note.date)}
+              {formatDistanceToNow(note.date, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
             </span>
             <p className="text-sm leading-6 text-slate-300">{note.content}</p>
           </div>
+
+          <button
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-small text-slate-300 outline-none font-medium group"
+          >
+            Do you want to <span className="text-red-400 group-hover:underline">delete this note</span>?
+          </button>
         </Dialog.Content>
-        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
       </Dialog.Portal>
     </Dialog.Root>
   );
